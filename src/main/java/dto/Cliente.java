6a5095207a21a182068898aa18368d2facbb6104
whoysAndre <yellowcata2121@gmail.com>
2025-05-23 10:23:58 -0500

    init

---
+++ src/main/java/dto/Cliente.java
@@ -0,0 +1,190 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dto;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author yello
+ */
+@Entity
+@Table(name = "cliente")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Cliente.findAll", query = "SELECT c FROM Cliente c"),
+    @NamedQuery(name = "Cliente.findByCodCli", query = "SELECT c FROM Cliente c WHERE c.codCli = :codCli"),
+    @NamedQuery(name = "Cliente.findByDniCli", query = "SELECT c FROM Cliente c WHERE c.dniCli = :dniCli"),
+    @NamedQuery(name = "Cliente.findByApaCli", query = "SELECT c FROM Cliente c WHERE c.apaCli = :apaCli"),
+    @NamedQuery(name = "Cliente.findByAmaCli", query = "SELECT c FROM Cliente c WHERE c.amaCli = :amaCli"),
+    @NamedQuery(name = "Cliente.findByNomCli", query = "SELECT c FROM Cliente c WHERE c.nomCli = :nomCli"),
+    @NamedQuery(name = "Cliente.findByFchNacCli", query = "SELECT c FROM Cliente c WHERE c.fchNacCli = :fchNacCli"),
+    @NamedQuery(name = "Cliente.findByLogiCli", query = "SELECT c FROM Cliente c WHERE c.logiCli = :logiCli"),
+    @NamedQuery(name = "Cliente.findByPasCli", query = "SELECT c FROM Cliente c WHERE c.pasCli = :pasCli")})
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "codCli")
+    private Integer codCli;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 8)
+    @Column(name = "dniCli")
+    private String dniCli;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "apaCli")
+    private String apaCli;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "amaCli")
+    private String amaCli;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "nomCli")
+    private String nomCli;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "fchNacCli")
+    @Temporal(TemporalType.DATE)
+    private Date fchNacCli;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "logiCli")
+    private String logiCli;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 500)
+    @Column(name = "pasCli")
+    private String pasCli;
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer codCli) {
+        this.codCli = codCli;
+    }
+
+    public Cliente(Integer codCli, String dniCli, String apaCli, String amaCli, String nomCli, Date fchNacCli, String logiCli, String pasCli) {
+        this.codCli = codCli;
+        this.dniCli = dniCli;
+        this.apaCli = apaCli;
+        this.amaCli = amaCli;
+        this.nomCli = nomCli;
+        this.fchNacCli = fchNacCli;
+        this.logiCli = logiCli;
+        this.pasCli = pasCli;
+    }
+
+    public Integer getCodCli() {
+        return codCli;
+    }
+
+    public void setCodCli(Integer codCli) {
+        this.codCli = codCli;
+    }
+
+    public String getDniCli() {
+        return dniCli;
+    }
+
+    public void setDniCli(String dniCli) {
+        this.dniCli = dniCli;
+    }
+
+    public String getApaCli() {
+        return apaCli;
+    }
+
+    public void setApaCli(String apaCli) {
+        this.apaCli = apaCli;
+    }
+
+    public String getAmaCli() {
+        return amaCli;
+    }
+
+    public void setAmaCli(String amaCli) {
+        this.amaCli = amaCli;
+    }
+
+    public String getNomCli() {
+        return nomCli;
+    }
+
+    public void setNomCli(String nomCli) {
+        this.nomCli = nomCli;
+    }
+
+    public Date getFchNacCli() {
+        return fchNacCli;
+    }
+
+    public void setFchNacCli(Date fchNacCli) {
+        this.fchNacCli = fchNacCli;
+    }
+
+    public String getLogiCli() {
+        return logiCli;
+    }
+
+    public void setLogiCli(String logiCli) {
+        this.logiCli = logiCli;
+    }
+
+    public String getPasCli() {
+        return pasCli;
+    }
+
+    public void setPasCli(String pasCli) {
+        this.pasCli = pasCli;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (codCli != null ? codCli.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Cliente)) {
+            return false;
+        }
+        Cliente other = (Cliente) object;
+        if ((this.codCli == null && other.codCli != null) || (this.codCli != null && !this.codCli.equals(other.codCli))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "dto.Cliente[ codCli=" + codCli + " ]";
+    }
+    
+}
---
+++ target/generated-sources/annotations/dto/Cliente_.java
@@ -0,0 +1,21 @@
+package dto;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2025-05-23T10:21:12")
+@StaticMetamodel(Cliente.class)
+public class Cliente_ { 
+
+    public static volatile SingularAttribute<Cliente, Integer> codCli;
+    public static volatile SingularAttribute<Cliente, String> dniCli;
+    public static volatile SingularAttribute<Cliente, String> apaCli;
+    public static volatile SingularAttribute<Cliente, Date> fchNacCli;
+    public static volatile SingularAttribute<Cliente, String> pasCli;
+    public static volatile SingularAttribute<Cliente, String> nomCli;
+    public static volatile SingularAttribute<Cliente, String> amaCli;
+    public static volatile SingularAttribute<Cliente, String> logiCli;
+
+}
